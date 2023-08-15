b22d2026f5fd9a22276987de60c3aec171d04ebd
daidai <dd891688848@163.com>
2023-08-15 15:50:14 +0800

    埋点更新

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -1,7 +1,15 @@
-<script>
+{/* <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-26FCKG3V7P');
-</script>
+</script> */}
+if (process.client) {
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-26FCKG3V7P');
+}
+  
